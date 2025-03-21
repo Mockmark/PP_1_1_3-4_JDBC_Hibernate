975540069a9d6fa837455fab878a585ea776b6b4
Michael Mark <selfdevourment@gmail.com>
2025-03-21 20:38:09 +0500

    Realize Util.getConnection

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/db";
+    private static final String USER = "AAAA";
+    private static final String PASSWORD = "impish donator versus uncouple bath subside";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver was not loaded. " + e.getMessage());
+        }
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            connection = conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed. " + e.getMessage());
+        }
+        return connection;
+    }
 }
