8bb77d4436c745a24fd9641136b5795a72eb1741
Michael Mark <selfdevourment@gmail.com>
2025-03-22 17:31:17 +0500

    Remove unused imports

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,8 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
